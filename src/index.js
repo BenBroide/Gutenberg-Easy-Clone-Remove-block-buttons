0a525ff88117c0c91e0e269b7eb4d8ee6d4125ba
Be Broide <be.broide@gmail.com>
2021-01-23 22:22:21 -0800

    init

--- src/index.js
+++ src/index.js
@@ -1,41 +1,66 @@
 import { compose, ifCondition } from '@wordpress/compose';
 import { registerFormatType } from '@wordpress/rich-text';
-import { RichTextToolbarButton } from '@wordpress/block-editor';
+import { RichTextToolbarButton,AlignmentToolbar } from '@wordpress/block-editor';
 import { withSelect } from '@wordpress/data';
 
 const MyCustomButton = props => {
 	console.log(props)
 	return <RichTextToolbarButton
 		icon='editor-code'
-		title='Sample output'
+		title='Test Button'
 		onClick={ () => {
 			console.log( 'toggle format' );
 		} }
 	/>
 };
 
-const ConditionalButton = compose(
-	withSelect( function( select ) {
-		return {
-			selectedBlock: wp.data.select( 'core/block-editor' ).getSelectedBlock
-		}
-	} ),
-	// ifCondition( function( props ) {
-	// 	console.log(props.selectedBlock.name)
-	// 	return (
-	// 		props.selectedBlock &&
-	// 		props.selectedBlock.name === 'core/paragraph'
-	// 	);
-	// } )
-)( MyCustomButton );
-document.addEventListener("DOMContentLoaded", function(event){
-	console.log('Yo')
-	registerFormatType(
-		'my-custom-format/sample-output', {
-			title: 'Sample output',
-			tagName: 'samp',
-			className: null,
-			edit: ConditionalButton,
-		}
+if(wp.hooks) {
+	const {addFilter} = wp.hooks;
+	const {Fragment} = wp.element;
+	const {InspectorControls,BlockControls} = wp.blockEditor;
+	const {createHigherOrderComponent} = wp.compose;
+	const {ToggleControl, PanelBody} = wp.components;
+
+	/**
+	 * Add mobile visibility controls on Advanced Block Panel.
+	 *
+	 * @param {function} BlockEdit Block edit component.
+	 *
+	 * @return {function} BlockEdit Modified block edit component.
+	 */
+	const withAdvancedControls = createHigherOrderComponent((BlockEdit) => {
+		return (props) => {
+
+			const {
+				name,
+				attributes,
+				setAttributes,
+				isSelected,
+			} = props;
+
+			// const {
+			// 	someAttr,
+			// } = attributes;
+
+			return (
+				<Fragment>
+					<BlockEdit {...props} >
+						<BlockControls>
+							<MyCustomButton/>
+						</BlockControls>
+					</BlockEdit>
+
+				</Fragment>
+			);
+		};
+	}, 'withAdvancedControls');
+
+
+
+	addFilter(
+		'editor.BlockEdit',
+		'editorskit/custom-advanced-control',
+		withAdvancedControls
 	);
-})
+}
+
