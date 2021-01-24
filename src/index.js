c7a9980bb5be4802416fcbcd922978ad9d352de3
Be Broide <be.broide@gmail.com>
2021-01-23 22:28:33 -0800

    init

--- src/index.js
+++ src/index.js
@@ -1,66 +1,31 @@
-import { compose, ifCondition } from '@wordpress/compose';
-import { registerFormatType } from '@wordpress/rich-text';
-import { RichTextToolbarButton,AlignmentToolbar } from '@wordpress/block-editor';
-import { withSelect } from '@wordpress/data';
+import { registerPlugin } from '@wordpress/plugins';
+import {
+	BlockControls,
+	ToolbarButton,
+	ToolbarGroup
+} from '@wordpress/components';
+import { check } from '@wordpress/icons';
 
-const MyCustomButton = props => {
-	console.log(props)
-	return <RichTextToolbarButton
-		icon='editor-code'
-		title='Test Button'
-		onClick={ () => {
-			console.log( 'toggle format' );
-		} }
-	/>
-};
+const BenNYCToolbar = ( props ) => {
+	const doSomething = () => {};
 
-if(wp.hooks) {
-	const {addFilter} = wp.hooks;
-	const {Fragment} = wp.element;
-	const {InspectorControls,BlockControls} = wp.blockEditor;
-	const {createHigherOrderComponent} = wp.compose;
-	const {ToggleControl, PanelBody} = wp.components;
-
-	/**
-	 * Add mobile visibility controls on Advanced Block Panel.
-	 *
-	 * @param {function} BlockEdit Block edit component.
-	 *
-	 * @return {function} BlockEdit Modified block edit component.
-	 */
-	const withAdvancedControls = createHigherOrderComponent((BlockEdit) => {
-		return (props) => {
-
-			const {
-				name,
-				attributes,
-				setAttributes,
-				isSelected,
-			} = props;
-
-			// const {
-			// 	someAttr,
-			// } = attributes;
-
-			return (
-				<Fragment>
-					<BlockEdit {...props} >
-						<BlockControls>
-							<MyCustomButton/>
-						</BlockControls>
-					</BlockEdit>
-
-				</Fragment>
-			);
-		};
-	}, 'withAdvancedControls');
-
-
-
-	addFilter(
-		'editor.BlockEdit',
-		'editorskit/custom-advanced-control',
-		withAdvancedControls
+	return (
+		<ToolbarGroup>
+			<ToolbarButton
+				icon={ check }
+				onClick={ doSomething }
+			/>
+		</ToolbarGroup>
 	);
 }
 
+registerPlugin(
+	'bennyc-toolbar-mods',
+	{
+		render: () => (
+			<BlockControls>
+				<BenNYCToolbar />
+			</BlockControls>
+		)
+	}
+);
